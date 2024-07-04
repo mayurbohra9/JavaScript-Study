28133b31dfdf20c64b540f76a2f94510170c9196
Mayur Bohra <71560105+mayurbohra9@users.noreply.github.com>
2024-07-04 17:06:54 +0000

    Conersions

---
+++ 01_Basics/03_conversions_operations.js
@@ -0,0 +1,61 @@
+let score = "20"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let score1 = "20abc"
+
+let valueInNumber = Number(score1)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "20" => 20
+// "20abc" => NaN
+// true => 1, false => 0
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+//"mayur" => true
+
+
+
+// ------------- Operations ------------
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+
+/* 
+console.log(2+2);   //addition
+console.log(2-2);   //substraction
+console.log(2/2);   //division
+console.log(2*2);   //multiplication
+console.log(2%2);   //modulus (remainder)
+console.log(2**3);  //2 power 3
+*/
+
+let str1 = "Mayur"
+let str2 = "Bohra"
+
+// console.log(str1 + " " + str2)
+
+/* 
+console.log("1" + 2);   //12
+console.log(1 + "2");   //12
+console.log("1" + 2 + 2);   //122
+console.log(1 + 2 + "2");   //32
+
+console.log(+true); //1
+console.log(+""); //0
+*/
+
