058354fbe4edb0f6ef79246469425f772f16ac2f
Mayur Bohra <71560105+mayurbohra9@users.noreply.github.com>
2024-07-18 05:29:42 +0000

    Compairison & datatypes

---
+++ 01_Basics/04_compairison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// comparing datatypes - always compare between same datatypes
+
+// console.log("2" > 1);  
+
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// "===" -> strict comparison which compares datatypes too with value
+
+console.log("2" === 2);  //false
+
+// "==" -> strict comparison values
+
+console.log("2" == 2);  //true
+
+
